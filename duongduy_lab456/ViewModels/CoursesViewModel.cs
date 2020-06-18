5c22af756946e0fa5e8760582010e8df67a9d0a5
doduongduy2711@gmail.com <doduongduy2711@gmail.com>
2020-06-19 03:26:30 +0700

    Add project files.

---
+++ duongduy_lab456/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using duongduy_lab456.ViewModels;
+using System.Globalization;
+using duongduy_lab456.Models;
+
+namespace duongduy_lab456.ViewModels
+{
+    public class CoursesViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+    }
+}
